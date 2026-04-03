f2a7aefdbd1a36bdebda1030e269308a9c345e2a
Fedor Romanov <fedor1378romanov@gmail.com>
2026-04-03 22:29:39 +0200

    edited signature

--- resources/views/components/signature.blade.php
+++ resources/views/components/signature.blade.php
@@ -1,3 +1,6 @@
 <footer {{ $attributes->merge(['class' => 'fixed bottom-0 left-0 w-full py-2 text-center text-sm shadow-sm text-muted-foreground']) }}>
-    <a href="https://www.linkedin.com/in/fedor-romanov">&copy; {{ date('Y') }} Fedor Romanov TE25A</a>
+    <div class="flex flex-col">
+        <span>&copy; {{ date('Y') }} Togethernet</span>
+        <a href="https://www.linkedin.com/in/fedor-romanov">Made by Fedor Romanov TE25A</a>
+    </div>
 </footer>
