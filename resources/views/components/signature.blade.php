317d3977e7fa5a312d416fc638ccc7edfd5b2693
Fedor Romanov <fedor1378romanov@gmail.com>
2026-04-20 22:45:32 +0200

    add underline to signature

--- resources/views/components/signature.blade.php
+++ resources/views/components/signature.blade.php
@@ -1,5 +1,5 @@
 <footer {{ $attributes->merge(['class' => 'mt-auto w-full py-4 text-center -mb-6 text-sm text-muted-foreground']) }}>
     <div class="flex flex-col">
-        <span>&copy; {{ date('Y') }} Togethernet och <a href="https://www.linkedin.com/in/fedor-romanov">Fedor Romanov</a>. Alla rättigheter förbehållna.</span>
+        <span>&copy; {{ date('Y') }} Togethernet och <a href="https://www.linkedin.com/in/fedor-romanov" class="underline">Fedor Romanov</a>. Alla rättigheter förbehållna.</span>
     </div>
 </footer>
