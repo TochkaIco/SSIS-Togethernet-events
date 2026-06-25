1046b42e99160028dc1a92bef621349bde853ed7
Fedor Romanov <fedor1378romanov@gmail.com>
2026-06-25 17:56:42 +0200

    Optimize settings.profile view and remove the button to pull profile picture from google if elevkar-auth is currently in use

--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -7,36 +7,27 @@
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
             @if(auth()->user()->profile_picture)
                 <flux:avatar :src="auth()->user()->profile_picture" :name="auth()->user()->name" :initials="auth()->user()->initials()" class="h-24 w-24 mb-2" circle />
-                <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your profile picture is handled by Google.') }}</flux:text>
-                <flux:button wire:click="pullPictureFromGoogle()" class="cursor-pointer" icon="arrow-path" variant="ghost">{{ __('Pull the picture from Google') }}</flux:button>
+                @if(App\Models\AppConfig::get('active_auth_provider', 'google') === 'google')
+                    <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your profile picture is handled by Google.') }}</flux:text>
+                    <flux:button wire:click="pullPictureFromGoogle()" class="cursor-pointer" icon="arrow-path" variant="ghost">{{ __('Pull the picture from Google') }}</flux:button>
+                @endif
             @else
                 <flux:avatar :name="auth()->user()->name" :initials="auth()->user()->initials()" class="h-24 w-24 mb-2" circle />
             @endif
 
             <div>
                 <flux:input wire:model="name" :label="__('Name')" type="text" disabled required autofocus autocomplete="name" />
-                <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your name is handled by Google and school LDAP. Contact Togethernet to change your name in the system.') }}</flux:text>
             </div>
 
             <div>
                 <flux:input wire:model="class" :label="__('Class')" type="text" disabled required />
-                <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your class is handled by school LDAP. Contact Togethernet to change your name in the system.') }}</flux:text>
             </div>
 
             <div>
                 <flux:input wire:model="email" :label="__('Email')" disabled />
-                <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your email is handled by Google.') }}</flux:text>
             </div>
 
-            <div class="flex items-center gap-4">
-                <div class="flex items-center justify-end">
-                    <flux:button variant="primary" type="button" class="w-full cursor-not-allowed">{{ __('Save') }}</flux:button>
-                </div>
-
-                <x-action-message class="me-3" on="profile-updated">
-                    {{ __('Saved.') }}
-                </x-action-message>
-            </div>
+            <flux:text class="mt-1 text-xs text-orange-400/80 cursor-default">{{ __('Your name, email and class are managed by Google / school\'s LDAP. Contact Togethernet to change your profile information in the system.') }}</flux:text>
         </form>
 
         @auth
