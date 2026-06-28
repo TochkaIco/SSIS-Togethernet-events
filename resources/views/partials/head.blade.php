d24dd6bf0f28c3dd135e83ab159d9d568297e471
Fedor Romanov <fedor1378romanov@gmail.com>
2026-06-28 03:20:16 +0200

    Fix meta data for open graph

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,17 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="Add a short, catchy description of your website here (around 150-160 characters) for Google search results.">
+
+<meta property="og:type" content="website">
+<meta property="og:title" content="{{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}">
+<meta property="og:description" content="{{ __('Togethernet is a :year-year-old organization founded and run by committed students at Stockholm Science & Innovation School to create fun events for all students.', ['year' => now()->subYears(2013)->format('y')]) }}">
+<meta property="og:image" content="{{ config('app.url') }}/images/togethernet-feature.jpg">
+<meta property="og:url" content="{{ config('app.url') }}">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="Hem - Togethernet">
+<meta name="twitter:description" content="Add a short, catchy description of your website here for X/Twitter.">
+<meta name="twitter:image" content="https://yourwebsite.com/path-to-your-image.jpg">
 
 <title>
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
