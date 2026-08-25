7b9ee07ae91e1440545a63d661997626c0fabb58
Fedor Romanov <fedor1378romanov@gmail.com>
2026-08-25 17:08:13 +0200

    Sentry frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,14 +9,19 @@ import ResizeImage from 'quill-resize-image';
 Quill.register('modules/resize', ResizeImage);
 window.Quill = Quill;
 
-Sentry.init({
-    dsn: import.meta.env.SENTRY_LARAVEL_DSN,
-    replaysSessionSampleRate: 1.0,
-    replaysOnErrorSampleRate: 1.0,
-    integrations: [
-        Sentry.replayIntegration({
-            maskAllText: true,
-            blockAllMedia: true,
-        }),
-    ],
-});
+const sentryDsn = document.querySelector('meta[name="sentry-dsn"]')?.getAttribute('content')
+    || import.meta.env.VITE_SENTRY_LARAVEL_DSN
+    || import.meta.env.VITE_SENTRY_DSN;
+if (sentryDsn) {
+    Sentry.init({
+        dsn: sentryDsn,
+        replaysSessionSampleRate: 1.0,
+        replaysOnErrorSampleRate: 1.0,
+        integrations: [
+            Sentry.replayIntegration({
+                maskAllText: true,
+                blockAllMedia: true,
+            }),
+        ],
+    });
+}
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -24,6 +24,8 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+<meta name="sentry-dsn" content="{{ config('sentry.dsn') }}">
+
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
 <style>
